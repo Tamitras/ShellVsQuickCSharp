7afbb4ffcea1df8b763512b7b0b1f68f9feeb90a
Erik <erik.kaufmann91@gmail.com>
2022-05-17 23:19:23 +0200

    GenerateRandomNumbers and Quick_Sort Function

---
+++ ShellVsQuickCSharp/Program.cs
@@ -0,0 +1,105 @@
+﻿using System.Numerics;
+
+namespace ShellVsQuickCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            List<ulong> numbers = new List<ulong>();
+
+            //var numbers = GenerateRandomNumbers(10); // 10^2 = 100
+            //var numbers = GenerateRandomNumbers(100); // 10^3 = 1.000
+
+            //numbers = GenerateRandomNumbers(1000); // 10^4 = 10.000
+            //DoQuickSort(numbers.ToArray());
+
+
+            //numbers = GenerateRandomNumbers(10000); // 10^5 = 100.000
+            //DoQuickSort(numbers.ToArray());
+
+
+            //numbers = GenerateRandomNumbers(100000); // 10^6 = 1.000.000
+            //DoQuickSort(numbers.ToArray());
+
+
+            //numbers = GenerateRandomNumbers(1000000); // 10^7 = 10.000.000
+            //DoQuickSort(numbers.ToArray());
+
+
+            //numbers = GenerateRandomNumbers(10000000); // 10^8 = 100.000.000
+            //DoQuickSort(numbers.ToArray());
+
+            //numbers = GenerateRandomNumbers(100000000); // 10^9 = 1.000.000.000
+            //DoQuickSort(numbers.ToArray());
+
+            numbers = GenerateRandomNumbers(1000000000); // 10^10 = 10.000.000.000
+            DoQuickSort(numbers.ToArray());
+
+            // Zerstört den Arbeitsspeicher!!!
+            //numbers = GenerateRandomNumbers(10000000000); // 10^11 = 10.000.000.000
+            //DoQuickSort(numbers.ToArray());
+        }
+
+        public static void DoQuickSort(ulong[] arr)
+        {
+            Console.WriteLine($"\n----------Starte sortieren von {arr.Length} Zahlen -------------------");
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            // the code that you want to measure comes here
+
+            Quicksort.Quick_Sort(arr, 0, (ulong)(arr.Length - 1));
+
+            watch.Stop();
+
+            decimal elapsedMs = watch.ElapsedMilliseconds;
+            var secs = Math.Round((elapsedMs / 1000),3);
+
+            Console.WriteLine($"Quicksort beended nach {elapsedMs}ms | {secs}secs");
+        }
+
+        private static ulong LongRandom(ulong min, ulong max, Random rand)
+        {
+            ulong result = (ulong)rand.Next((Int32)(min >> 32), (Int32)(max >> 32));
+            result = (result << 32);
+            result = result | (ulong)rand.Next((Int32)min, (Int32)max);
+            return result;
+        }
+
+        private static List<ulong> GenerateRandomNumbers(ulong potenz)
+        {
+            List<ulong> numbers = new List<ulong>();
+            Array arr = Array.CreateInstance(typeof(ulong), (int)(10 * potenz));
+
+            Console.WriteLine("\n----------Generiere Zufallszahlen-------------------");
+
+            Random rd = new Random();
+
+            for (ulong i = 0; i < (ulong)(10 * potenz); i++)
+            {
+
+                if((ulong)numbers.Count == (ulong)((10*potenz)-1))
+                {
+                    break;
+                }
+
+                //ulong rand_num = (ulong)rd.Next(1, (ulong)(10 * potenz));
+                ulong rand_num = LongRandom(1, (10 * potenz), rd);
+
+                numbers.Add(rand_num);
+
+                //if (!numbers.Contains(rand_num))
+                //{
+                //    numbers.Add(rand_num);
+                //}
+                //else
+                //{
+                //    i--;
+                //}
+            }
+
+            return numbers;
+        }
+    }
+}
---
+++ ShellVsQuickCSharp/Quicksort.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShellVsQuickCSharp
+{
+    public static class Quicksort
+    {
+        public static void Quick_Sort(UInt64[] arr, UInt64 left, UInt64 right)
+        {
+            if (left < right)
+            {
+                UInt64 pivot = Partition(arr, left, right);
+
+                if (pivot > 1)
+                {
+                    Quick_Sort(arr, left, pivot - 1);
+                }
+                if (pivot + 1 < right)
+                {
+                    Quick_Sort(arr, pivot + 1, right);
+                }
+            }
+
+        }
+
+        private static UInt64 Partition(UInt64[] arr, UInt64 left, UInt64 right)
+        {
+            UInt64 pivot = arr[left];
+            while (true)
+            {
+
+                while (arr[left] < pivot)
+                {
+                    left++;
+                }
+
+                while (arr[right] > pivot)
+                {
+                    right--;
+                }
+
+                if (left < right)
+                {
+                    if (arr[left] == arr[right]) return right;
+
+                    UInt64 temp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = temp;
+
+
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+    }
+}
